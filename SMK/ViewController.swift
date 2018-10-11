366f9e3ee386339e7ee764ff9a3c1cffa8f07730
Вячеслав Рожнов <v.rozhnov@gazenergoinform.ru>
2018-10-11 12:23:26 +0300

    Initial Commit

---
+++ SMK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMK
+//
+//  Created by Вячеслав Рожнов on 11.10.2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
